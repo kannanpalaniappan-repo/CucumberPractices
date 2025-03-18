9a00bcb356f172ecc7d20e05cba627fa84a3e887
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-03-18 20:56:10 +0530

    Cucumber (BDD) with Java - Write your first #Cucumber Test with (#Gherkin) Feature File

---
+++ src/main/java/AmazonImplementation/Product.java
@@ -0,0 +1,43 @@
+package AmazonImplementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+
+	private String productName;
+	private Integer price;
+	
+	public Product(String productName, Integer price){
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+	public List<String> getProductList(){
+		List<String> productList = new ArrayList<>();
+		
+		productList.add("Apple MacBook Pro");
+		productList.add("Apple MacBook Air");
+		productList.add("Apple iPhone 12");
+		
+		return productList;
+		
+	}
+}
